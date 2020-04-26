4419986d4f2e8e9b1f3655c6505e38bb4090e870
Syed Saad Ullah Shah <shahsaadullah11@gmail.com>
2020-04-27 04:40:32 +0500

    ScrollView Added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -32,9 +32,9 @@ export default function App() {
         onPress={addTextHandler}
         />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => <View key={goal} style={styles.listItems}><Text>{goal}</Text></View>)}
-      </View>  
+      </ScrollView>  
     </View>
   );
 }
