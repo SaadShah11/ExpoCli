c9238ae7d4ab268821f488ea3ef309b8aba7edc7
Syed Saad Ullah Shah <shahsaadullah11@gmail.com>
2020-04-27 04:31:39 +0500

    List added

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredText, setEnteredText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const textInputtHandler = (userText) => {
+    setEnteredText(userText)
+  }
+
+  const addTextHandler = () => {
+    //Adding old array to new then adding new item
+    setCourseGoals(currentGoals => [...currentGoals, enteredText])
+  }
+
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder="Enter Task" style={styles.button}/>
-        <Button title="Add"/>
+      <View style={styles.innerContainer}>
+        <TextInput 
+        placeholder="Enter Task" 
+        style={styles.textArea}
+        //Using this means run function in every key stroke
+        onChangeText={textInputtHandler}
+        value={enteredText}
+        />
+        <Button 
+        title="Add" 
+        style={styles.button}
+        //Paranthesis not used so that it will execute only when button pressed
+        onPress={addTextHandler}
+        />
       </View>
       <View>
-        
+        {courseGoals.map((goal) => <View key={goal} style={styles.listItems}><Text>{goal}</Text></View>)}
       </View>  
     </View>
   );
@@ -17,12 +41,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    //flex: 1,
+    paddingTop: 50
+  },
+  innerContainer: {
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   button: {
-    padding: 20
+    padding: 20,
+    borderRadius: 1,
+  },
+  textArea: {
+    width: 120
+  },
+  listItems: {
+    padding: 10,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    backgroundColor: '#709BD7',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
